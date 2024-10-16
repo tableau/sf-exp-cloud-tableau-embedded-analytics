c7547cea5d791e8804c02a0925513b819e9a0f0c
jrunrun <jpcraycraft@gmail.com>
2024-10-16 14:22:01 -0500

    updateds to token class and vizEmbedsidebar

--- force-app/main/default/lwc/vizEmbedSidebar/vizEmbedSidebar.js
+++ force-app/main/default/lwc/vizEmbedSidebar/vizEmbedSidebar.js
@@ -7,7 +7,7 @@
 
 // Load the Tableau Embed v3 API in the head markup
 // https://help.salesforce.com/s/articleView?id=sf.community_builder_page_head.htm&type=5
-// You can use Head Markup in Builder -> Settigngs -> Advanced -> Edit Head Markup
+// You can use Head Markup in Builder -> Settigns -> Advanced -> Edit Head Markup
 // <script defer type="module" src="https://us-west-2b.online.tableau.com/javascripts/api/tableau.embedding.3.latest.js"></script>
 
 
@@ -19,15 +19,44 @@ import {
 import generateJWT from '@salesforce/apex/CATokenGenerator.generateJWT';
 import getUserDetails from '@salesforce/apex/CATokenGenerator.getUserDetails';
 import getViews from '@salesforce/apex/CATokenGenerator.getViews';
-import getTabEnv from '@salesforce/apex/CATokenGenerator.getTabEnv';
+import getTableauEnvConfig from '@salesforce/apex/CATokenGenerator.getTableauEnvConfig';
 import getCurrentUserOpportunities from '@salesforce/apex/CATokenGenerator.getCurrentUserOpportunities';
 import { NavigationMixin } from 'lightning/navigation';
 // import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { loadScript } from 'lightning/platformResourceLoader';
+
 
 export default class VizEmbedSidebar extends NavigationMixin(LightningElement)  {
     @track showCustomToast = false;
-    @track message = '  Returns have increased above the expected range for the current time period.  ';
+    @track toastMessage = '  Returns have increased above the expected range for the current time period.  ';
+    @track isOpen = false;
+    @track isTextVisible = false;
+    @track jwt;
+    @track userDetails;
 
+    viz;
+    sheet;
+    targetSheet;
+    tokenType = 'SSO';
+    contentUrl;
+    views;
+    viewUrl;
+    server;
+    site;
+    workbook;
+    view;
+    path = '/t/';
+    params = '?:embed=y';
+    opportunities;
+    baseUrl;
+    SCRIPT_PATH;
+    SCRIPT_PATH_ALT;
+    isInitialized = false; // Flag to prevent multiple executions
+    sentNotification = false;
+    responseModel;
+    notificationString;
+    rawResponse;
+    
     handleCloseToast() {
         console.log('close event received in vizEmbedSidebar component!');
         this.showCustomToast = false; // Hide the toast when the close event is triggered
@@ -36,9 +65,6 @@ export default class VizEmbedSidebar extends NavigationMixin(LightningElement)  
 
 
     // Sidebar stuff
-    @track isOpen = false;
-    @track isTextVisible = false;
-
     get svgColor() {
         /* menu text color for SVG */
         return '#C23335';
@@ -58,54 +84,57 @@ export default class VizEmbedSidebar extends NavigationMixin(LightningElement)  
 
     }
 
-    handleVizItem2(event) {
+    async handleVizItem2(event) {
         console.log('vizitem2 event fired!');
         this.contentUrl = event.detail;
-        this.initViz(true);
+        console.log('contentUrl @vizEmbedSidebar:', this.contentUrl);
+        await this.initViz(true);
 
-
-        // This is cool. Instead of calling initViz, we just update the src property on the Tableau web component which is more performant. Need to implement this in CDAPP
-        // this.tableauContainer.src = this.vizUrl;
     }
 
-    @track jwt;
-    @track userDetails;
-
-    viz;
-    sheet;
-    targetSheet;
-    tokenType = 'SSO';
-    contentUrl;
-    views;
-    viewUrl;
-    server;
-    site;
-    workbook;
-    view;
-    path = '/t/';
-    params = '?:embed=y';
-    opportunities;
-    
-    connectedCallback() {
+    async init() {
 
         this.template.addEventListener('navigatetopulse', () => {
             console.log('navigatetopulse event received in vizEmbedSidebar component!');
-            this[NavigationMixin.Navigate]({
-                type: 'standard__component',
-                attributes: {
-                    componentName: 'c__pulseEmbedSidebar'
-                }
-            });
+            try {
+                this[NavigationMixin.Navigate]({
+                    type: 'standard__webPage',
+                    attributes: {
+                        url: '/metrics'
+                    }
+                });
+                console.log('Navigation to /metrics initiated.');
+            } catch (error) {
+                console.error('Navigation failed:', error);
+            }
         });
 
-        getCurrentUserOpportunities()
-        .then(result => {
-            console.log('opportunities: ' + JSON.stringify(result));
-            this.opportunities = result;
-            console.log('opportunities: ' + JSON.stringify(this.opportunities));
-            return getTabEnv();
-        })
-      
+        // Get the current domain dynamically
+        this.baseUrl = window.location.origin;
+        console.log('Current Domain: ' + this.baseUrl);
+
+        this.SCRIPT_PATH = '/js/tableau/tableau.embedding.3.9.0-pre.21.min.js';
+        this.SCRIPT_PATH_ALT = '/sfsites/c' + this.SCRIPT_PATH;
+
+        // Load the Tableau Embed v3 API script
+        loadScript(this, this.baseUrl + this.SCRIPT_PATH)
+            .then(() => {
+                console.log('Tableau Embed v3 API script loaded successfully.');
+            })
+            .catch(() => {
+                console.log('Tableau Embed v3 API script failed to load using ' + this.SCRIPT_PATH + ' - now trying ' + this.SCRIPT_PATH_ALT);
+                return loadScript(this, this.baseUrl + this.SCRIPT_PATH_ALT);
+            })
+            .then(() => {
+                // This will execute after either the initial loadScript (success or fail)
+
+                return getCurrentUserOpportunities();
+            })
+            .then(result => {
+                this.opportunities = result;
+                console.log('Authenticated User\'s Opportunities: ' + JSON.stringify(this.opportunities, null, 2));
+                return getTableauEnvConfig();
+            })
             .then(result => {
                 // Handle the result from the second call
                 console.log('tabEnv: ' + JSON.stringify(result));
@@ -158,7 +187,30 @@ export default class VizEmbedSidebar extends NavigationMixin(LightningElement)  
                 // Handle error
                 console.log("error: " + JSON.stringify(error));
             });
- 
+    }
+
+    // connectedCallback() {
+    //     console.log('connectedCallback fired!');
+    //     console.log('The current value of this.isInitialized is: ' + this.isInitialized);
+    //     if (!this.isInitialized) { 
+    //         this.init();
+    //         this.isInitialized = true;
+    //         console.log('Initializing component, and setting this.isInitialized to: ' + this.isInitialized);
+    //     } else {
+    //         console.log('connectedCallback previously called so skipping init()');
+    //     }
+    // }
+
+    renderedCallback() {
+        console.log('renderedCallback fired!');
+        console.log('The current value of this.isInitialized is: ' + this.isInitialized);
+        if (!this.isInitialized) {
+            this.init();
+            this.isInitialized = true;
+            console.log('Initializing component, and setting this.isInitialized to: ' + this.isInitialized);
+        } else {
+            console.log('renderedCallback previously called so skipping init()');
+        }
     }
 
     // showToastNotification(title, message, variant) {
@@ -172,13 +224,13 @@ export default class VizEmbedSidebar extends NavigationMixin(LightningElement)  
     // }
 
     get tableauContainer() {
-        const container = this.template.querySelector('.tableau-target-div');
-        console.log('tableauContainer: ' + JSON.stringify(container));
+        console.log('tableauContainer fired!');
+        // console.log('tableauContainer: ' + JSON.stringify(this.template.querySelector('.tableau-target-div')));
         return this.template.querySelector('.tableau-target-div');
     }
 
     get vizUrl() {
-        
+        console.log('vizUrl fired!');
         if (this.contentUrl) {
             let parts = this.contentUrl.split('/');
             this.view = parts[parts.length - 1];
@@ -196,7 +248,13 @@ export default class VizEmbedSidebar extends NavigationMixin(LightningElement)  
     // In the initViz1() method, we create the viz object from the HTML element (container), and then use the viz object to programatically configure the embedded viz.
     // This approach requires the <tableau-viz> web component to be included in the HTML
     async initViz(fromMenuClick) {
+        console.log('initViz fired!');
         const container = this.tableauContainer;
+
+        console.log('fromMenuClick:', fromMenuClick);
+        console.log('this.contentUrl:', this.contentUrl);
+        console.log('this.vizUrl:', this.vizUrl);
+        console.log('container:', container);
 
         if (fromMenuClick) {
             console.log('Generating JWT after menu click');
@@ -221,14 +279,23 @@ export default class VizEmbedSidebar extends NavigationMixin(LightningElement)  
             container.token = this.jwt;
             container.src = this.vizUrl;
             // container.debug = true;
-            container.toolbar = 'top';
-            // container.toolbar = 'hidden';
+            // container.toolbar = 'top';
+            container.hideTabs = true;
+            container.toolbar = 'hidden';
 
             console.log('container.token: ' + container.token);
             console.log('container.src: ' + container.src);
 
             let tableauVizElement = this.template.querySelector('[data-id="tableauViz"]');
+
+            tableauVizElement.addEventListener('vizloaderror', (errorEvent) => {
+                console.log('error loading viz');
+                const message = JSON.parse(errorEvent.detail.message);
+                console.log('error message @ viz load: ' + message);
+                // console.log('error: ' + JSON.stringify(errorEvent.detail.message));
+            });
             
+      
             // Listen for the 'firstinteractive' event, which is fired when the viz has finished loading
             tableauVizElement.addEventListener('firstinteractive', (onFirstInteractiveEvent) => {
                 this.showCustomToast = true;
